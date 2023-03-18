74da80b814d52ae410a6f139e709613d1d38cb26
James Blackwell <jwblackwell@gmail.com>
2023-03-18 19:39:22 +0000

    update

--- src/config/artisan-ai.php
+++ src/config/artisan-ai.php
@@ -1,7 +1,11 @@
 <?php
 
 return [
+    // get this here: https://platform.openai.com/account/api-keys
     'openai_api_key' => env('OPENAI_API_KEY', ''),
+
+    // strongly recommend you use gpt-4, though it will technically work with gpt-3.5-turbo
     'model' => env('OPENAI_MODEL', 'gpt-4'),
+
     'timeout' => env('OPENAI_TIMEOUT', 60),
 ];
